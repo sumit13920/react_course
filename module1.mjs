4362837aa66a35940033c6c914cff5167e489595
sumit13920 <sumitthakur6500@gmail.com>
2024-10-22 02:19:21 +0530

    Initial commit

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import dz,{a,c,d} from './module2.mjs'
+console.log(dz);
+console.log(a);
+console.log(c);
+console.log(d);
+//ye hamara pehla react component hone wala hai. To mithai batwa to apne saher me.
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "SitaRam";
+const b = "Hanuman"
+const c = "BakraTundam"
+const d = "Ekdantam"
+
+export default b;
+export {a};
+export {c};
+export {d};
